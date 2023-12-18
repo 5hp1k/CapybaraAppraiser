168cc4304781f10dc8282eaf50ea3584ceaa8c13
5hpik <maxim.loboda2013@yandex.ru>
2023-12-19 02:36:57 +0700

    Работа с БД 2

--- db_item.py
+++
@@ -1,8 +0,0 @@
-class Item:
-    """Класс записи базы данных"""
-    def __init__(self, title, url, opinion='Neutral', comment=''):
-        self.title = title
-        self.url = url
-        self.opinion = opinion
-        self.comment = comment
-        
